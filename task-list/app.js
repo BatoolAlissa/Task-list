be5f01b569ef3221894d34e4b12488bae9a8bc7d
BatoolAlissa <43234063+BatoolAlissa@users.noreply.github.com>
2019-03-28 18:38:18 +0200

    first commit'

---
+++ task-list/app.js
@@ -0,0 +1,7 @@
+//defined the UI variables 
+
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.cleat-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task')
